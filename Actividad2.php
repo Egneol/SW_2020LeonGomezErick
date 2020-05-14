23314670779bae32a7045358dd74081b06415c95
Egneol <legoerick@comunidad.unam.mx>
2020-05-14 13:06:32 -0500

    FEAT: Actividad2.php

---
+++ Actividad2.php
@@ -0,0 +1,3 @@
+<?php
+  echo "Erick León Gómez</br>Me gusto aprender Sesiones porque creo que es algo muy util y otra cosa que creo que me gustara aprender sera javascript o al menos tengo ganas de aprenderlo</br> me gusto mucho la clase de hoy :)";
+?>
